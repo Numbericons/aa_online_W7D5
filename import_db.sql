589757c78b97742aa615527a59f7b4c5354fe49b
ZachO <zachjoliver@gmail.com>
2019-05-03 14:14:24 -0700

    Finish phase 2 working phase 3

--- import_db.sql
+++ import_db.sql
@@ -3,46 +3,42 @@ DROP TABLE IF EXISTS cats;
 DROP TABLE IF EXISTS toys; 
 
 CREATE TABLE cats (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     color VARCHAR(255),
     breed VARCHAR(255)
 );
 
 CREATE TABLE toys ( 
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     price INTEGER,
     color VARCHAR(255),
     name VARCHAR(255)
 );
 
--- CREATE TABLE cattoys (
---     id INTEGER PRIMARY KEY,
---     cat_id INTEGER NOT NULL,
---     toy_id INTEGER NOT NULL,
---     FOREIGN KEY (cat_id) REFERENCES cats(id),
---     FOREIGN KEY (toy_id) REFERENCES toys(id)
--- );
+CREATE TABLE cattoys (
+    id SERIAL PRIMARY KEY,
+    cat_id INTEGER NOT NULL,
+    toy_id INTEGER NOT NULL,
+    FOREIGN KEY (cat_id) REFERENCES cats(id),
+    FOREIGN KEY (toy_id) REFERENCES toys(id)
+);
 
 
 INSERT INTO cats (name, breed, color)
 VALUES ('Hobbes', 'calico', 'orange'),
-VALUES ('Scooter', 'persian', 'black'),
-VALUES ('Scrawny', 'siamese', 'white'),
-VALUES ('Poopmachine', 'ginger', 'orange'),
-VALUES ('Lucy', 'domestic', 'nude');
+       ('Scooter', 'persian', 'black'),
+       ('Scrawny', 'siamese', 'white'),
+       ('Poopmachine', 'ginger', 'orange'),
+       ('Lucy', 'domestic', 'nude');
 
 INSERT INTO toys (price, color, name)
 VALUES (25, 'black', 'flogger'),
-VALUES (10, 'red', 'curtain'),
-VALUES (100, 'grey', 'tower'),
-VALUES (50, 'purple', 'grape_suit'),
-VALUES (150, 'brick red', 'handcuffs');
+       (10, 'red', 'curtain'),
+       (100, 'grey', 'tower'),
+       (50, 'purple', 'grape_suit'),
+       (150, 'brick red', 'handcuffs');
 
--- INSERT INTO cattoys (cat_id, toy_id)
--- VALUES  
--- VALUES
--- VALUES
--- VALUES
--- VALUES
+INSERT INTO cattoys (cat_id, toy_id)
+VALUES (1,5), (1,1), (2,4), (3,4), (4,3), (5,2);
 
